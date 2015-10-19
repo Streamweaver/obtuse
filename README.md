926a6095b93b66f81917e633ee15b235c58ef121
Scott Turnbull <Streamweaver@gmail.com>
2015-10-19 10:52:20 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Overview
+========
+
+Just a playground app for angularjs.
+
+Setup Dev
+---------
+
+To setup a quick dev env with live-server:
+
+```
+npm init -y
+npm i live-server --save-dev
+```
+
+Modify package.json to scripts has "start": "live-server"
+
+To run:
+
+```
+npm start
+```
---
+++ assets/js/stellardata.js
@@ -0,0 +1,42 @@
+var planets = [
+  {
+    name: "Mercury",
+    surfaceGravity: 0.38,
+    au: 0.39
+  },
+  {
+    name: "Venus",
+    surfaceGravity: 0.38,
+    au: 0.723
+  },
+  {
+    name: "Earth",
+    surfaceGravity: 1.00,
+    au: 1.000
+  },
+  {
+    name: "Mars",
+    surfaceGravity: 1.52,
+    au: 1.524
+  },
+  {
+    name: "Jupiter",
+    surfaceGravity: 0.38,
+    au: 5.203
+  },
+  {
+    name: "Saturn",
+    surfaceGravity: 0.38,
+    au: 9.539
+  },
+  {
+    name: "Uranus",
+    surfaceGravity: 0.38,
+    au: 19.180
+  },
+  {
+    name: "Neptune",
+    surfaceGravity: 0.38,
+    au: 30.530
+  },
+];
